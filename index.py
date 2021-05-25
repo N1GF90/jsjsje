724603ffba19a52dfc034f4b51d3675ef05e491e
Edwin Christie <edwinchristie100@gmail.com>
2021-05-24 22:36:53 -0400

    Updated Code
    Changed title, added requirments.txt

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_file
 from pytube import YouTube
 from os import path
+import io
 import os
 
 app = Flask(__name__)
@@ -17,14 +18,11 @@ def download():
     if request.method == 'POST':
         downloadLink = request.form['downloadURL']
         local_download_path = YouTube(downloadLink).streams.filter(progressive=True, file_extension='mp4').get_highest_resolution().download()
-        fname = local_download_path.split("//")[-1]
-        return send_file(fname, as_attachment=True)
-
-@app.after_request
-def after_request(response):
-    if request.endpoint == "download":
-        global local_download_path
-        if path.exists(local_download_path):
-            os.remove(local_download_path)
-            print("File Removed Succesfully")
-    return response
+        #Here we are storing the downloaded file in memory so that we can delete it later from the OS
+        return_data = io.BytesIO()
+        with open(local_download_path, 'rb') as fo:
+            return_data.write(fo.read())
+        # (after writing, cursor will be at last byte, so move it to start)
+        return_data.seek(0)
+        os.remove(local_download_path)
+        return send_file(return_data, mimetype='video/mp4',  attachment_filename='download_video.mp4', as_attachment=True)
